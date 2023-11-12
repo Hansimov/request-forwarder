ca9bcba64b96af1a35dcf6e3c1136c82f9f8bda4
Hansimov <591172499@qq.com>
2023-11-13 02:40:08 +0800

    :gem: [Feature] Enable forward post request with http-proxy-middleware

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 app.use(cors());
 
-app.use("/forward/*", function (req, res, next) {
-    const target = req.params[0];
-    console.log(target);
+app.use(
+    "/forward",
     createProxyMiddleware({
-        target: target,
+        target: "https://copilot.github1s.tk",
         changeOrigin: true,
-    })(req, res, next);
-});
+        pathRewrite: {
+            "^/forward/": "",
+        },
+    })
+);
 
 const port = 12444;
 app.listen(port);
