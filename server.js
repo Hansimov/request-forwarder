6f63fe35263e9aab35c6c55797d8b3cb8d7e0895
Hansimov <591172499@qq.com>
2023-11-13 00:17:01 +0800

    :zap: [Enhance] Generalize request params

--- server.js
+++ server.js
@@ -17,22 +17,10 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.post("/forward/*", (req, res) => {
-    let forward_url = req.body.url;
-    let forward_method = req.body.method || "POST";
-    let forward_headers = req.body.headers || {};
-    let forward_body = req.body.body || {};
-    console.log(`forward_url: ${forward_url}`);
-    console.log(`forward_method: ${forward_method}`);
-    console.log(`forward_headers: ${JSON.stringify(forward_headers)}`);
-    console.log(`forward_body: ${JSON.stringify(forward_body)}`);
-    request({
-        url: forward_url,
-        method: forward_method,
-        headers: forward_headers,
-        body: forward_body,
-        json: true,
-    }).pipe(res);
+app.post("/forward", (req, res) => {
+    let options = req.body;
+    console.log(`Forward request: ${JSON.stringify(options)}`);
+    request(options).pipe(res);
 });
 
 let port = 12444;
